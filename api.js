acf49e5e504a834c23165cb2fd96115ba0082989
Rawnly <fedevitale99@gmail.com>
2017-07-07 15:46:42 +0200

    update

--- api.js
+++ api.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const got = require('got');
+const Ora = require('ora');
+
+const spinner = new Ora('Uploading');
+
 const url = {
 	up: "https://api.imgur.com/3/image"
 };
@@ -8,8 +12,9 @@ module.exports = {
 	upload: _upload,
 }
 
-function _upload(filename, options = { client_id: '775904bf84b8658' }) {
+function _upload(filename, options = { client_id: '67f0f72978a1369' }) {
 	return new Promise(function(resolve, reject) {
+		spinner.start();
 		fs.readFile(filename, (err, data) => {
 			const img = {
 				title: options.title ? options.title : filename.split('.')[0],
@@ -26,8 +31,10 @@ function _upload(filename, options = { client_id: '775904bf84b8658' }) {
 					'Authorization': `Client-ID ${options.client_id}`
 				}
 			}).then(({body}) => {
+				spinner.succeed();
 				resolve(body.data)
 			}).catch(e => {
+				spinner.fail();
 				reject(e);
 			})
 		});
